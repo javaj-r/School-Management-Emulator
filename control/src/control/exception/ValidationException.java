6b267cb30aee4133405c590edb7f3de4634f8d40
Javid Rafiee <javid.rafiee@gmail.com>
2022-01-27 01:06:58 +0330

    Add ValidationException.

---
+++ control/src/control/exception/ValidationException.java
@@ -0,0 +1,19 @@
+package control.exception;
+
+/**
+ * @author javid
+ * Created on 1/27/2022
+ */
+public class ValidationException extends RuntimeException {
+
+    public ValidationException() {
+    }
+
+    public ValidationException(String message) {
+        super(message);
+    }
+
+    public ValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
